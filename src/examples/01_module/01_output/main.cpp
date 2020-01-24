bf83ae4f6ee131ad0f75c7be629db97aead8b9e3
Connor Stack <connor.stack@g.austincc.edu>
2020-01-23 19:03:39 -0600

    show include differences

--- src/examples/01_module/01_output/main.cpp
+++ src/examples/01_module/01_output/main.cpp
@@ -1,9 +1,11 @@
-#include "output.h"
+#include "output.h"//"" for including things that we create
+ #include <iostream>// <> for things from the library
 
+ using std::cout;
 
 int main() 
-{
-	
+{ 
+	cout<<"Hello World!"
 
 	return 0;
 }
