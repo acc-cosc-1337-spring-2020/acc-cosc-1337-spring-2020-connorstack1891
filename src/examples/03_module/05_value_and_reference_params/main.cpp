1baa51c8e06a01977d114133779999b37154beed
Connor Stack <connor.stack@g.austincc.edu>
2020-02-13 18:31:10 -0600

    ref, value, and const  parameters

--- src/examples/03_module/05_value_and_reference_params/main.cpp
+++ src/examples/03_module/05_value_and_reference_params/main.cpp
@@ -1,7 +1,15 @@
 #include<iostream>
 
+using std::cout;
+
 int main() 
 {
+	int num = 5;
+	cout << "value of num is: " << num << " address is " << &num; 
+
+	int &num_ref = num;
+	num_ref = 10;
+	cout << "\n value of num is: " << num << " address is " << &num;
 
 	return 0;
 }
