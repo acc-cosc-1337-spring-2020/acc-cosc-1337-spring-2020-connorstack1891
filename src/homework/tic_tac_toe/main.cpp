8f2bafc48ad3c7abcbd4d3d22db4ac6cb0aaf440
Connor Stack <connor.stack@g.austincc.edu>
2020-03-05 16:25:21 -0600

    HW5

--- src/homework/tic_tac_toe/main.cpp
+++ src/homework/tic_tac_toe/main.cpp
@@ -1,6 +1,15 @@
+#include "tic_tac_toe.h"
+#include <iostream>
+
+using std::cout; using std::cin;
 
 int main() 
 {
+	auto first_player;
+
+	cout << "First player";
+	cin >> first_player;
+
 
 	return 0;
 }
--- src/homework/tic_tac_toe/tic_tac_toe.h
+++ src/homework/tic_tac_toe/tic_tac_toe.h
@@ -1,1 +1,24 @@
 //h
+#include <string>
+
+class TicTacToe
+{
+public:
+	void start_game(std::string first_player);
+	void mark_board(int position);
+	string get_player()const;
+private:
+	void set_next_player();
+
+};
+
+
+class Error
+{
+public:
+	Error(std::string msg) : message{ msg } {}
+	std::string get_message()const { return message; }
+
+private:
+	std::string message;
+};
