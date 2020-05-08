b66d62f226ebc862ddbe07d9c3209442d2c52253
Connor Stack <connor.stack@g.austincc.edu>
2020-05-07 19:05:04 -0500

    stack char array

--- src/examples/12_module/06_arrays_char/arrays_char.cpp
+++ src/examples/12_module/06_arrays_char/arrays_char.cpp
@@ -1,1 +1,25 @@
 //cpp
+#include"arrays_char.h"
+#include<iostream>
+
+using namespace std;
+
+void char_array()
+{
+	const int SIZE = 5;
+	char name[SIZE] = "mary";
+	name[4] = '\0'; 
+	cout << name << "\n";
+
+	for (int i = 0; name[i] != '\0'; ++i)
+	{
+		cout << name[i] << "\n";
+	}
+}
+
+void char_array_no_size()
+{
+	char name[] = "mary";
+	cout << name << "\n";
+}
+
--- src/examples/12_module/06_arrays_char/main.cpp
+++ src/examples/12_module/06_arrays_char/main.cpp
@@ -1,8 +1,9 @@
-
+#include "arrays_char.h"
 
 int main()
 {
-	
+	//char_array();
+	char_array_no_size();
 
 	return 0;
 };
